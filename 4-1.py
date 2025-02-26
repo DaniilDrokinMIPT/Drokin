724f02a5f02eb9fa2e9d11fdc24a69adf1205a60
DaniilDrokinMIPT <drokin.dv@phystech.edu>
2025-02-26 10:16:19 +0300

    Add files via upload

---
+++ 4-1.py
@@ -0,0 +1,36 @@
+import RPi.GPIO as GPIO
+
+
+dac = [8, 11, 7, 1, 8, 5, 12, 6]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+GPIO.output(dac, 0)
+
+
+def dec2bin(val):
+    return [int(x) for x in bin(val)[2:].zfill(8)]
+
+
+try:
+    while True:
+        num = input()
+        if num == 'q':
+            print("Interrupt")
+            break
+
+        try:
+            num = int(num)
+            if num < 0:
+                print("Negative")
+            elif num > 255:
+                print("Limit")
+            else:
+                num2 = dec2bin(num)
+                GPIO.output(dac, num2)
+                u = num2 * 3.3 / 255
+                print(u)
+        except ValueError:
+            print('No num')
+finally:
+    GPIO.output(dac, 0)
+    GPIO.cleanup()
---
+++ 4-2.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+
+dac = [8, 11, 7, 1, 8, 5, 12, 6]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(dac, GPIO.OUT)
+
+
+def dec2bin(val):
+    return [int(x) for x in bin(val)[2:].zfill(8)]
+
+
+try:
+    p = float(input('Enter interval'))
+
+    while True:
+        for num in range(256):
+            num2 = dec2bin(num)
+            GPIO.output(dac, num2)
+            time.sleep(p / 512)
+finally:
+    GPIO.output(dac, 0)
+    GPIO.cleanup()
